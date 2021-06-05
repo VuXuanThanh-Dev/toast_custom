452da8e767357b1b02c280b2d80eff2532dd9cd0
VuXuanThanh-Dev <devxuanthanhvu0503@gmail.com>
2021-06-05 12:52:36 +0700

    update delete toast

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let arrToast = [];
+
 const deleteBtn = document.querySelector(".delete");
 const success = document.querySelector(".btn-success");
 const warning = document.querySelector(".btn-warning");
@@ -6,11 +9,12 @@ const body = document.querySelector("body");
 let container = document.createElement('div');
     container.classList.add('container');
 
-    
+
 success.addEventListener("click", successToast);
 warning.addEventListener("click", warningToast);
 error.addEventListener("click", errorToast);
 deleteBtn.addEventListener("click", deleteToast);
+
 
 function successToast() {
   createToast('success', 'Thành công!', 'đăng nhập thành công!', 3000);
@@ -24,24 +28,27 @@ function errorToast() {
     createToast('error', 'Lỗi', 'đăng nhập không thành công!', 3000);
 }
 
-function deleteToast(e) {
+function deleteToast(idx) {
+    arrToast[idx].remove();
 }
 
 
 function createToast(type, header, content, timelife) {
-    
+
     let toast = document.createElement("div");
     toast.classList.add("toast");
     toast.classList.add(type);
+    arrToast.push(toast);
     toast.innerHTML = `
     <div class="body">
     <p class="header">${header}!</p>
     <p class="content">${content}</p>
     </div>
-    <div class="delete" >X</div>`;
+    <div class="delete" onclick="deleteToast(${arrToast.length-1})">X</div>`;
     container.appendChild(toast);
     body.appendChild(container);
     setTimeout(() => {
+        arrToast = arrToast.filter(toast => toast != toast);
         toast.remove();
     }, timelife);
 }
